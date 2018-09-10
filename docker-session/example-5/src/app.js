35fb7cb0386c201970e55669577c4fc0dcbd7723
Ayush Ghosh <ayush.ghosh@appdynamics.com>
2018-09-10 11:08:22 +0530

    example 5 add

---
+++ docker-session/example-5/src/app.js
@@ -0,0 +1,14 @@
+
+
+
+
+
+require("http").createServer(function(request, response) {
+	
+	
+    response.writeHead(200, { "Content-Type": "text/plain" });
+    response.write("Hello World!!!");
+    response.end();
+	    
+}).listen(3000);
+console.log('listening on port 3000');
